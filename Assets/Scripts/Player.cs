968a6eba40433878c25d809732085326a6e27111
brfwind <19172405320@163.com>
2025-11-20 16:26:34 +0800

    feat:初始成品

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using TMPro;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     public Rigidbody rb;
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI textComponent;
+    public GameObject winTextGo;
+    public float speed;
+    public float Score = 0;
 
-    void Update()
+    void FixedUpdate()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
-        Vector3 dir = new Vector3(h,0,v);
+        Vector3 dir = new Vector3(h,0,v) * speed;
 
         rb.AddForce(dir);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("退出游戏"); // 在编辑器里测试用
+            Application.Quit();     // 打包后才有效
+        }
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Food")
+        {
+            Score++;
+            textComponent.text = "Score:" + Score.ToString();
+            Destroy(collision.gameObject);
+
+            if(Score == 15)
+            {
+                winTextGo.SetActive(true);
+            }
+        }
     }
 }
